070344d925da4aa9c82309c870ff66ab3b27e086
Jan Ranostaj <HonzaRanostaj@gmail.com>
2025-04-18 07:51:24 +0200

    save progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { Link } from "expo-router";
+import Button from "@/components/Button";
 
-export default function Search() {
+export default function SearchScreen() {
   return (
-    <View className={"flex-1 justify-center items-center bg-[#25292e]"}>
-      <Text className={"text-5xl text-white font-bold"}>Explore Screen</Text>
-      <Link href={"/tours"} className={"text-white text-xl p-2 underline"}>
-        Tours
-      </Link>
-      <Link href={"/profile"} className={"text-white text-xl p-2 underline"}>
-        Profile
-      </Link>
-      <Link href={"/about"} className={"text-white text-xl p-2 underline"}>
-        About
-      </Link>
+    <View className={"flex-1 items-center bg-[#25292e]"}>
+      <View>
+        <TextInput
+          className="bg-[#222630] px-4 py-3 outline-none w-[280px] text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+          placeholder="Enter email or username"
+        />
+      </View>
+      <Text className={"text-white text-xl p-2"}>Search Link</Text>
+      <Text className={"text-white text-xl p-2"}>List of Cards</Text>
+      <Text className={"text-white text-xl p-2"}>Card 1</Text>
+      <Text className={"text-white text-xl p-2"}>Card 2</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       // TODO: change custom theme
       colors: {
-        primary: '#030014',
-        accent: '#AB8BFF'
-      }
+        primary: "#030014",
+        accent: "#AB8BFF",
+      },
     },
   },
   plugins: [],
-}
+};
